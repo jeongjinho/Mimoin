fd0a4e5b5f5fbc5096d46e3253edef6835dacee5
jeong jin ho <yyyqor@gmail.com>
2019-10-06 17:25:46 +0900

    프로젝트생성

---
+++ Mimoin/Mimoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mimoin
+//
+//  Created by 진호놀이터 on 06/10/2019.
+//  Copyright © 2019 진호놀이터. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
